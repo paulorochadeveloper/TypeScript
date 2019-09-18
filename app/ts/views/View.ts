defcbc4a0134cdb3769adf3fa59d97dcd70a4112
Paulo Rocha <paulorochaweb@bitbucket.org>
2019-09-18 10:20:45 +0100

    #FINISH CURSO TYPESCRIPT1 - TYPES-JQUERY

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,16 +1,16 @@
 abstract class View<T> {
 
     // mudou para protected!
-    protected _elemento: Element;
+    protected _elemento: JQuery;
 
     constructor(seletor: string) {
 
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T) {
 
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     abstract template(model: T): string ;
